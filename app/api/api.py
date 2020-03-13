256b6ac3f4b377064eee43e8c7abeab3fc04c889
creedowl <creedowl@gmail.com>
2020-03-13 20:07:10 +0800

    feat(submission): change router prefix to "/submissions"

--- app/api/api.py
+++ app/api/api.py
@@ -5,7 +5,7 @@ from app.api.endpoints import authentication, submissions, users, labs
 router = APIRouter()
 router.include_router(authentication.router, prefix="/auth",
                       tags=["authentication"])
-router.include_router(submissions.router, prefix="/submit", tags=["submit"])
+router.include_router(submissions.router, prefix="/submissions", tags=["submit"])
 router.include_router(users.router, prefix="/users", tags=["users"])
 router.include_router(labs.router, prefix="/labs", tags=["labs"])
 
