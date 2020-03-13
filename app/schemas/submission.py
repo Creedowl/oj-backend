535ba558025f9738dfb0bd4f8f454c3bcde3be89
creedowl <creedowl@gmail.com>
2020-03-13 20:08:19 +0800

    feat(submission): update submission model and schema

--- app/schemas/submission.py
+++ app/schemas/submission.py
@@ -5,8 +5,21 @@ class SubmissionOut(BaseModel):
     id: int
     origin_filename: str
     user_id: int
+    lab_id: int
+    code: str
     status: str
+    total_score: int = None
     result: str = None
+    log: str = None
 
     class Config:
         orm_mode = True
+
+
+class SubmissionUpdate(BaseModel):
+    id: int
+    filename: str
+    status: str
+    total_score: int
+    result: str
+    log: str
